a83d0644ea5695b141584897e68333215d14bd0e
rahulvenkat207 <rahulvenkat207@gmail.com>
2025-11-19 23:37:49 +0530

    close

--- app.py
+++
@@ -1,8 +0,0 @@
-def main():
-    """
-    A simple function that returns a greeting.
-    """
-    return "Hello, World!"
-
-if __name__ == "__main__":
-    print(main())
--- test.py
+++
@@ -1,7 +0,0 @@
-from app import main
-
-def test_main():
-    """
-    Tests the main function from app.py.
-    """
-    assert main() == "Hello, World!"
